834bae43ba6ca9caf83b4d685583b34d33f9198a
juney <juhnnie@gmail.com>
2019-07-22 21:50:42 -0700

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,8 +1,7 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: "4eecf61c6e564080ae8fdfd89b13eafb",
+  secret: "010bff2321c74cc78dff895b6ddb21e5"
 };
-
-var keys = require("./keys.js");    
+ 
